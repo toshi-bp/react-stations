aaca73134289ef8a0ce416ca1d6d914146cdc715
toshi-bp <yttoshi-bp@outlook.jp>
2021-08-23 15:17:59 +0900

    station6

--- src/App.js
+++ src/App.js
@@ -10,21 +10,32 @@ import './App.css'
  */
 
 const header = <header>Dogアプリ</header>;
-const description = <p>犬の画像を表示するサイトです</p>
+const description = <p>犬の画像を表示するサイトです</p>;
 
 function DogImage() {
-  const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/lhasa/n02098413_7358.jpg");
+  const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/greyhound-italian/n02091032_1121.jpg");
+  const API_URL = "https://dog.ceo/api/breeds/image/random";
+
+  function componentDidMount() {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setDogUrl(result.message)
+        }
+      )
+  }
 
   return (
     <div>
       <img src={dogUrl} />
       <p>This is a dog.</p>
-      <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/greyhound-italian/n02091032_1121.jpg")}>
+      <button onClick={() => componentDidMount()}>
         change a dog
       </button>
     </div>
   );
-}
+};
 
 export const App = () => {
   return (
