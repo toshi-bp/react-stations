6ce0e9062f7ec2d54b30d32c357afd761304d697
toshi-bp <yttoshi-bp@outlook.jp>
2021-08-23 14:08:58 +0900

    station3

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ import './App.css'
  */
 
 const header = <header>Dogアプリ</header>;
+const description = <p>犬の画像を表示するサイトです</p>
+const dog_img = <img src="https://images.dog.ceo/breeds/lhasa/n02098413_7358.jpg" alt="dog"></img>
 
 export const App = () => {
   return (
     <div>
       {header}
+      {description}
+      {dog_img}
     </div>
   )
 }
