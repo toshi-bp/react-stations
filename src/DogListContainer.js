02f798308d95248f0f3dc938895210ef2a4d4399
toshi-bp <yttoshi-bp@outlook.jp>
2021-09-01 01:06:22 +0900

    station9

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,21 @@
 // DO NOT DELETE
+import React, { useEffect } from 'react'
+import './App.css'
+
+export function DogListContainer(props) {
+    const [breeds, setBreeds] = React.useState()
+
+    useEffect(() => {
+        breeds = fetch(props.url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    result.message
+                }
+            )
+    })
+
+    return (
+        <div></div>
+    )
+};
