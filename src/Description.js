dab032081bd0c389fae5664c6a3989096addcccf
toshi-bp <yttoshi-bp@outlook.jp>
2021-08-31 22:26:57 +0900

    staion8

--- src/Description.js
+++ src/Description.js
@@ -4,11 +4,13 @@ import * as React from 'react'
 import './App.css'
 import { DogImage } from './DogImage'
 
+const imageUrl = "https://dog.ceo/api/breeds/image/random"
+
 export function Description() {
     return (
         <div>
             <p>犬の画像を表示するサイトです</p>
-            <DogImage url="https://dog.ceo/api/breeds/image/random" />
+            <DogImage url={imageUrl} />
         </div>
     );
 };
