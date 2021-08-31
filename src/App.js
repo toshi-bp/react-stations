09c80d60620089b3bb61c7bdebe6d78c456494b8
toshi-bp <yttoshi-bp@outlook.jp>
2021-09-01 01:10:14 +0900

    station9

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import * as React from 'react'
 import './App.css'
 import { Header } from './Header'
 import { Description } from './Description';
+import { DogListContainer } from './DogListContainer';
 
 /**
  * 
  * @type {React.FC}
  */
 
+const API_URL = ""
+
 export const App = () => {
   return (
     <div>
       <Header />
       <Description />
+      <DogListContainer url={API_URL}/>
     </div>
   )
 }
