e9c75b423205e83d87223feabd38476ea6942235
toshi-bp <tahara.toshimasa.yv@mynavi.jp>
2021-09-01 02:15:08 +0000

    station9

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react'
 import './App.css'
 
-export function DogListContainer(props) {
+export function DogListContainer() {
     const API_URL = "https://dog.ceo/api/breeds/list/all"
     const [breeds, setBreeds] = React.useState([])
 
