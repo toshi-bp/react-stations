843e77aa815cf181eb0b90d18267f5b982bebd16
toshi-bp <tahara.toshimasa.yv@mynavi.jp>
2021-09-01 02:19:15 +0000

    station9

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import * as React from 'react'
 import './App.css'
 import { Header } from './Header'
 import { Description } from './Description';
-import { DogListContainer } from './DogListContainer';
 
 /**
  * 
@@ -16,7 +15,6 @@ export const App = () => {
     <div>
       <Header />
       <Description />
-      <DogListContainer />
     </div>
   )
 }
